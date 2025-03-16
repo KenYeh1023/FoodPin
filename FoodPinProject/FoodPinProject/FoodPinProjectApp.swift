df6f97ce5f872802239fd38dd65db0a1672e7d72
Ken Yeh <s1011451@gmail.com>
2025-03-16 23:28:16 +0800

    Initial Commit

---
+++ FoodPinProject/FoodPinProject/FoodPinProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinProjectApp.swift
+//  FoodPinProject
+//
+//  Created by Ken on 2025/3/16.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
